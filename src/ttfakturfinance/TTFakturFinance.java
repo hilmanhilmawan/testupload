e75fdd2f3c04d248c20b358170307e75b8e29d62
Iqbal Indra <iqbal.indra@j-express.id>
2017-07-02 00:50:18 +0700

    Nambahin Sysout

--- src/ttfakturfinance/TTFakturFinance.java
+++ src/ttfakturfinance/TTFakturFinance.java
@@ -16,6 +16,7 @@ public class TTFakturFinance {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello");
     }
     
 }
