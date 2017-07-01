c8a4ec5568aa5aa3374a3842e675c613dba50b88
Iqbal Indra <iqbal.indra@j-express.id>
2017-07-02 00:42:25 +0700

    First Commint

---
+++ src/ttfakturfinance/TTFakturFinance.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ttfakturfinance;
+
+/**
+ *
+ * @author j-express.id
+ */
+public class TTFakturFinance {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
